e7c72bd2f1421d1a726b15c75c8c36aff8204b09
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-12 17:48:36 -0300

    google auth api setup

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,18 @@
+// setup providers -- google authentication
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {},
+});
+
+export { handler as GET, handler as POST };

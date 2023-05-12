1bde78895de5ccde337c168604e3fb2969638ec8
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-12 12:18:54 -0300

    homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,23 @@
-// unique file which will render home page
+import Feed from "@components/Feed";
 
+// unique file which will render home page
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          AI is here, for mostly better than worse
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Pomptop - a place to discover and share AI prompts
+      </p>
+
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
